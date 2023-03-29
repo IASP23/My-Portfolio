4a6d41b8bb1c33f5de310d5c02044578c918450c
Ismael2301 <iasimbana1@espe.edu.ec>
2023-03-29 17:39:17 -0500

    SideBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { CardHome } from "./components/card/card";
 import { NavbarVertical } from "./components/navBar/navbarVertical";
+import Sidebar from "./components/navbarKaren/navbarK";
 function App() {
   return (
     <div className="App">
       <NavbarVertical />
       <div className="container p-4">
         <CardHome />
+        <Sidebar />
       </div>
     </div>
   );
